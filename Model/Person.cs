22e45c6a5d97bbebd5cef37b2238b61e6164eb77
fiipealan <filipealan@gmail.com>
2021-04-01 11:41:22 -0300

    Faltou essa classe

--- Model/Person.cs
+++ Model/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +8,12 @@ using System.Threading.Tasks;
 namespace Model
 {
     public class Person:Entity
-    {    
+    {   
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Campo Obrigatório não preenchido",ErrorMessageResourceName ="Nome",ErrorMessageResourceType =typeof(string))]
         public string Name { get; set; }
+        [RegularExpression()]
+        public string Email { get; set; }
         public DateTime BirthDate { get; set; }
+                
     }
 }
